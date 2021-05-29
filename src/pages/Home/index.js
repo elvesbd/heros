51ab089ae924158a38c6d0248440b606e75aef2b
elves brito <elvesbd@yahoo.com>
2021-05-29 19:39:33 -0300

    added background-image page home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,21 @@
+import styles from './styles.module.scss';
+
+
 export default function Home() {
   return (
-    <h1>Home</h1>
+    <section className={styles.homeContainer}>
+      <div className={styles.homeContainerVertical}>
+        <div className={styles.homeContainerHorizontal}>
+          <div className={styles.contentContainer}>
+            <span>👏 Olá, Bem-Vindo</span>
+            <h1>Novas noticías sobre a <span>Marvel</span></h1>
+            <p>
+              Marvel revela datas e nomes dos filmes da nova fase do universo cinematográfico.
+            </p>
+          </div>
+        </div>
+      </div>
+
+    </section>
   );
 };
