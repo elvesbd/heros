44af40c85cf5fd8e559052b26f538941b4037979
elves brito <elvesbd@yahoo.com>
2021-05-29 15:53:39 -0300

    created component header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,13 +1,19 @@
+import { SignInButton } from '../SignInButton';
+import styles from './styles.module.scss';
+
+
 export function Header() {
   return (
-    <header>
-      <div>
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
         <img src="/images/logo.svg" alt="Marvel"/>
 
         <nav>
-          <a>Home</a>
+          <a className={styles.active}>Home</a>
           <a>Characters</a>
         </nav>
+
+        <SignInButton />
       </div>
     </header>
   );
