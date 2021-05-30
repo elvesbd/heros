7de663b167468330c0fed0735bf0a756b4550b39
elves brito <elvesbd@yahoo.com>
2021-05-30 20:48:02 -0300

    configuring variables ambient

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 import md5 from 'md5';
 
-const publicKey = 'a4e21bb2dc85b749cfba2b9310910b6c';
-const privateKey = 'caab23674d64ea95dcf186f091de987312f8c0fb';
+const publicKey = process.env.REACT_APP_MARVEL_PUBLIC_KEY;
+const privateKey = process.env.REACT_APP_MARVEL_PRIVATE_KEY;
 const ts = Number(new Date());
 
 const hash = md5(ts + privateKey + publicKey);
